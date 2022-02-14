4e02b2b307518c989525d8c92ea41caca754480c
0ettam02 <matteoaruta6@gmail.com>
2022-02-14 15:34:47 +0100

    calcolo probabilita

---
+++ git mei/calcolo probabilita.py
@@ -0,0 +1,14 @@
+print ('\nDi cosa vuoi calcolare la probabilità?')
+variabili = print ('mercato azionario')
+qualcosa = input()
+if qualcosa == 'mercato azionario' :
+    print ('\ndigita valuta')
+    valuta = input()
+else :
+    print ('sium')
+print ('\ndigita il valore di apertura e chiusura degli ultimi 5 giorni')   
+uno = input('apertura 1 ')
+due = input('apertura 2 ')
+tre = input('apertura 3 ')
+quattro = input('apertura 4 ')
+cinque = input('apertura 5 ')
